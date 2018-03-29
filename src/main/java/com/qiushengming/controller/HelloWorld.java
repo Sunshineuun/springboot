17b58f052498d7b70433e9840f679eb7937f605c
qiushengming@aliyun.com <sunshine>
2018-03-29 17:10:37 +0800

    Initial commit

---
+++ src/main/java/com/qiushengming/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.qiushengming.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by qiushengming on 2018/3/29.
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/helloworld")
+    public String helloworld() {
+        return "hellword";
+    }
+}
