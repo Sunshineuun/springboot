7e713fa80ee1c10501c17dfacb6941115340ddee
qiushengming@aliyun.com <sunshine>
2018-03-29 17:55:04 +0800

    hello world

--- src/main/java/com/qiushengming/SpringbootApplication.java
+++ src/main/java/com/qiushengming/SpringbootApplication.java
@@ -2,8 +2,17 @@ package com.qiushengming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        RedisAutoConfiguration.class,
+        MongoAutoConfiguration.class,
+        SecurityAutoConfiguration.class})
 public class SpringbootApplication {
 
     public static void main(String[] args) {
