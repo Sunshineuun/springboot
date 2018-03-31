79484d157e495b1d65a182fcc2e96171b5f0498b
MinNie930625 <qiushengming@aliyun.com>
2018-03-31 21:46:46 +0800

    所有实体的基础类 为了序列化实现Serializable

---
+++ src/main/java/com/qiushengming/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.qiushengming.entity;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * 要缓存的 Java 对象必须实现 Serializable 接口，因为 Spring 会将对象先序列化再存入 Redis。
+ * 如果不实现 Serializable 的话将会遇到类似这种错误
+ * <code>
+ *     DefaultSerializer requires a Serializable payload but received an
+ *     object of type [com.qiushengming.entity.User]
+ * </code>
+ * @author MinMin
+ * @date 18年03月31日
+ */
+public class BaseEntity implements Serializable {
+
+    /**
+     * 由UUID构成
+     */
+    private String id = UUID.randomUUID().toString();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
