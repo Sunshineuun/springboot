15c5f59aa2dad3344d318d97c682e4eda69b75ca
MinNie930625 <qiushengming@aliyun.com>
2018-05-07 13:27:08 +0800

    请求路径资源定向处理

---
+++ src/main/java/com/qiushengming/controller/ForwardController.java
@@ -0,0 +1,43 @@
+package com.qiushengming.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 请求路径定向处理
+ * @author qiushengming
+ * @date 2018/5/7
+ */
+@Controller
+public class ForwardController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/forward/**")
+    public String forward(HttpServletRequest request) {
+        String contextPath = request.getContextPath();
+        String requestURI = request.getRequestURI();
+
+        logger.info("当前请求的基地址：{}，uri：{}", contextPath, requestURI);
+
+        String ctx = contextPath + "/forward/";
+        String path =
+            requestURI.substring(requestURI.indexOf(ctx) + ctx.length());
+
+        char symbol1 = '.';
+        String symbol2 = "/";
+
+        if (path.lastIndexOf(symbol1) > 0) {
+            path = path.substring(0, path.lastIndexOf('.'));
+        } else if (path.endsWith(symbol2)) {
+            path = path + "index";
+        }
+
+        logger.info("访问路径：{}", path);
+        return path;
+    }
+}
