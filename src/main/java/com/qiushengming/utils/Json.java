173d9485dd65579b58c93b791b44b5ad83503e32
Sunshineuun <qiushengming@aliyun.com>
2018-05-30 17:44:38 +0800

    json转换调整

--- src/main/java/com/qiushengming/utils/Json.java
+++ src/main/java/com/qiushengming/utils/Json.java
@@ -3,6 +3,8 @@ package com.qiushengming.utils;
 import com.google.gson.Gson;
 import com.qiushengming.entity.BaseEntity;
 
+import java.util.Map;
+
 /**
  * @author qiushengming
  * @date 2018/4/16
@@ -10,7 +12,11 @@ import com.qiushengming.entity.BaseEntity;
 public class Json {
     private static final Gson GSON = new Gson();
 
-    public static String toJson(BaseEntity o) {
+    public static String objectToJson(BaseEntity o) {
         return GSON.toJson(o);
+    }
+
+    public static Map stringToMap(String s) {
+        return GSON.fromJson(s, Map.class);
     }
 }
