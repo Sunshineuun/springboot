bbc96c9e8a7c78c9a9fb6c9cded400ef8cbdc420
Sunshineuun <qiushengming@aliyun.com>
2018-06-13 11:13:53 +0800

    通过js格式化json的函数

--- src/main/resources/static/js/app/commons.js
+++ src/main/resources/static/js/app/commons.js
@@ -68,7 +68,7 @@ function download_file(url, params) {
 function formatter(dictionaryKey, value) {
   var values = dictionary[dictionaryKey];
 
-  if (!value || !values){
+  if (!value || !values) {
     return "";
   }
 
@@ -78,4 +78,79 @@ function formatter(dictionaryKey, value) {
   }
 
   return value;
+}
+
+function repeat(s, count) {
+  return new Array(count + 1).join(s);
+}
+
+/**
+ * 格式化json
+ */
+function formatJson(json) {
+
+  var i = 0,
+    len = 0,
+    tab = "    ",
+    targetJson = "",
+    indentLevel = 0,
+    inString = false,
+    currentChar = null;
+
+
+  for (i = 0, len = json.length; i < len; i += 1) {
+    currentChar = json.charAt(i);
+
+    switch (currentChar) {
+      case '{':
+      case '[':
+        if (!inString) {
+          targetJson += currentChar + "\n" + repeat(tab, indentLevel + 1);
+          indentLevel += 1;
+        } else {
+          targetJson += currentChar;
+        }
+        break;
+      case '}':
+      case ']':
+        if (!inString) {
+          indentLevel -= 1;
+          targetJson += "\n" + repeat(tab, indentLevel) + currentChar;
+        } else {
+          targetJson += currentChar;
+        }
+        break;
+      case ',':
+        if (!inString) {
+          targetJson += ",\n" + repeat(tab, indentLevel);
+        } else {
+          targetJson += currentChar;
+        }
+        break;
+      case ':':
+        if (!inString) {
+          targetJson += ": ";
+        } else {
+          targetJson += currentChar;
+        }
+        break;
+      case ' ':
+      case "\n":
+      case "\t":
+        if (inString) {
+          targetJson += currentChar;
+        }
+        break;
+      case '"':
+        if (i > 0 && json.charAt(i - 1) !== '\\') {
+          inString = !inString;
+        }
+        targetJson += currentChar;
+        break;
+      default:
+        targetJson += currentChar;
+        break;
+    }
+  }
+  return targetJson;
 }
