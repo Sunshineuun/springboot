d514847e9a48e72b533a7c38e3d6348faad9c5b0
Sunshineuun <qiushengming@aliyun.com>
2018-06-30 21:27:57 +0800

    addViewControllers方法重写 contrller映射到view上

--- src/main/java/com/qiushengming/configuration/CorsConfiguration.java
+++ src/main/java/com/qiushengming/configuration/CorsConfiguration.java
@@ -3,6 +3,7 @@ package com.qiushengming.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -22,6 +23,13 @@ public class CorsConfiguration {
                     .allowedHeaders("*")
                     .allowedMethods("*")
                     .allowedOrigins("*");
+            }
+
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/index").setViewName("index");
+                registry.addViewController("/").setViewName("index");
+                registry.addViewController("/login").setViewName("login");
             }
         };
     }
