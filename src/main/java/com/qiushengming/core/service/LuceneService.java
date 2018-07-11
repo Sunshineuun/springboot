275a7746935c2ba045897e402f6e36f7567237c9
qiushengming@aliyun.com <sunshine>
2018-07-11 15:35:10 +0800

    lucene功能优化

--- src/main/java/com/qiushengming/core/service/LuceneService.java
+++ src/main/java/com/qiushengming/core/service/LuceneService.java
@@ -1,9 +1,45 @@
 package com.qiushengming.core.service;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+
+import java.beans.IntrospectionException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 /**
  * @author qiushengming
  * @date 2018/7/6
  */
 public interface LuceneService {
+    /**
+     * 增加一个document
+     *
+     * @param o 实体
+     * @return 插入成功的第几位
+     * @throws IOException               IO异常
+     * @throws IntrospectionException    反射引发的异常
+     * @throws InvocationTargetException 反射引发的异常
+     * @throws IllegalAccessException    反射引发的异常
+     */
+    long add(Object o) throws IOException, IntrospectionException, InvocationTargetException, IllegalAccessException;
+
+    /**
+     * 删除所有
+     */
+    void delAll();
+
+    /**
+     * 查询
+     *
+     * @param field 查询的字段
+     * @param value 查询的值
+     * @return {@link Document}
+     * @throws ParseException 解析异常
+     * @throws IOException    IO异常
+     */
+    List<Document> search(String field, String value) throws
+            ParseException, IOException;
 }
 
--- src/main/java/com/qiushengming/core/service/impl/LuceneServiceImpl.java
+++ src/main/java/com/qiushengming/core/service/impl/LuceneServiceImpl.java
@@ -1,8 +1,10 @@
 package com.qiushengming.core.service.impl;
 
 import com.qiushengming.core.service.LuceneService;
+import com.qiushengming.lucene.analyzer.IKAnalyzer4Lucene7;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
@@ -14,15 +16,21 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 疾病资料库
@@ -30,20 +38,20 @@ import java.nio.file.Paths;
  * @author qiushengming
  * @date 2018/7/6
  */
+@Slf4j
 @Service("luceneService")
 public class LuceneServiceImpl
-    implements LuceneService, InitializingBean {
+        implements LuceneService, InitializingBean {
     /**
      * 文件的路径
      */
     @Value("${minnie.lucene.filepath}")
     private String filePath;
-    private final Logger logger = LoggerFactory.getLogger(LuceneServiceImpl.class);
     private FSDirectory directory;
     /**
      * 分词器
      */
-    private Analyzer analyzer = new StandardAnalyzer();
+    private Analyzer analyzer = new IKAnalyzer4Lucene7();
 
     /**
      * 写入配置
@@ -65,20 +73,53 @@ public class LuceneServiceImpl
     public LuceneServiceImpl() throws IOException {
     }
 
-
     /**
      * 增加
      *
-     * @param field 字段
-     * @param value 值
+     * @param bean 实体
      * @throws IOException IO异常
      */
-    private void add(String field, String value) throws IOException {
+    @Override
+    public long add(Object bean) throws IOException, IntrospectionException,
+            InvocationTargetException, IllegalAccessException {
         Document document = new Document();
-        document.add(new TextField(field, value, Field.Store.YES));
-        document.add(new TextField(field + "1", String.valueOf((int) value.charAt(0)), Field.Store.YES));
+        BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
 
-        logger.info("插入成功的数量: {}", getWriter().addDocument(document));
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor descriptor : propertyDescriptors) {
+            String propertyName = descriptor.getName();
+            if (!StringUtils.equals(propertyName, "class")) {
+                Method readMethod = descriptor.getReadMethod();
+                if (!readMethod.getReturnType().equals(String.class)) {
+                    log.debug("{}方法的返回值不为String类型，我能期待是String类型，或者定义好了toString方法", readMethod.getName());
+                    continue;
+                }
+                Object result = readMethod.invoke(bean);
+
+                if (null == result) {
+                    result = "";
+                }
+
+                document.add(new TextField(propertyName, result.toString(), Field.Store.YES));
+            }
+        }
+
+        long result = getWriter().addDocument(document);
+        getWriter().flush();
+        getWriter().commit();
+        /*log.debug("插入成功的数量: {}", );*/
+        return result;
+    }
+
+    @Override
+    public void delAll() {
+        try {
+            getWriter().deleteAll();
+            getWriter().flush();
+            getWriter().commit();
+        } catch (IOException e) {
+            log.error("{}", e);
+        }
     }
 
     /**
@@ -90,10 +131,10 @@ public class LuceneServiceImpl
         try {
             Document document = new Document();
             document.add(new TextField(field, newValue, Field.Store.YES));
-            logger.info("更新的返回结果：{}", getWriter().updateDocument(new Term(field, value), document));
+            log.info("更新的返回结果：{}", getWriter().updateDocument(new Term(field, value), document));
             getWriter().commit();
         } catch (IOException e) {
-            logger.error("{}", e);
+            log.error("{}", e);
         }
     }
 
@@ -107,10 +148,10 @@ public class LuceneServiceImpl
         try {
             QueryParser queryParser = new QueryParser(field, analyzer);
             Query query = queryParser.parse(value);
-            logger.info("删除的返回结果：{}", getWriter().deleteDocuments(query));
+            log.info("删除的返回结果：{}", getWriter().deleteDocuments(query));
             getWriter().commit();
         } catch (IOException e) {
-            logger.error("{}", e);
+            log.error("{}", e);
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -124,7 +165,9 @@ public class LuceneServiceImpl
      * @throws ParseException 异常
      * @throws IOException    异常
      */
-    private void search(String field, String value) throws ParseException, IOException {
+    @Override
+    public List<Document> search(String field, String value) throws
+            ParseException, IOException {
         // 多参数查询
         //Query q = MultiFieldQueryParser.parse(new String[]{},new String[]{},new StandardAnalyzer());
 
@@ -132,18 +175,24 @@ public class LuceneServiceImpl
         QueryParser queryParser = new QueryParser(field, analyzer);
         Query query = queryParser.parse(value);
 
-        TopDocs topDocs = getSearcher().search(query, 10);
+        TopDocs topDocs = getSearcher().search(query, 100);
 
         long conut = topDocs.totalHits;
-        System.out.println("检索总条数：" + conut);
+        /*System.out.println("检索总条数：" + conut);*/
         ScoreDoc[] scoreDocs = topDocs.scoreDocs;
-        for (ScoreDoc scoreDoc : scoreDocs) {
-            Document document = getSearcher().doc(scoreDoc.doc);
-            logger.info(
-                "相关度：" + scoreDoc.score + "-----time:" + document.get(field) + "----char:" + document.get(field + "1"));
-            logger.info(document.get(field));
-        }
 
+        List<Document> result = new ArrayList<>();
+        for (ScoreDoc scoreDoc : scoreDocs) {
+            // TODO 暂时把相关度低于5的给过滤掉
+            if (scoreDoc.score < 5) {
+                log.info("相关度：{},过滤当前查询结果。", scoreDoc.score);
+                continue;
+            }
+            Document doc = getSearcher().doc(scoreDoc.doc);
+            result.add(doc);
+            /*log.info(document.get(field));*/
+        }
+        return result;
     }
 
     private IndexWriter getWriter() throws IOException {
@@ -169,23 +218,23 @@ public class LuceneServiceImpl
 
     private void testPath() {
         Path path = Paths.get(filePath);
-        logger.info("根路径:{}", path.getRoot());
-        logger.info("path中包含的路径数量:{}", path.getNameCount());
+        log.info("根路径:{}", path.getRoot());
+        log.info("path中包含的路径数量:{}", path.getNameCount());
 
         Path absolutePath = path.toAbsolutePath();
-        logger.info("绝对路径:{}", absolutePath.toString());
-        logger.info("绝对路径下包含的路径数量:{}", absolutePath.getNameCount());
+        log.info("绝对路径:{}", absolutePath.toString());
+        log.info("绝对路径下包含的路径数量:{}", absolutePath.getNameCount());
 
         System.out.println(path);
     }
 
     private void check() {
         try {
-            logger.info("有效的索引文档:" + getReader().numDocs());
+            log.info("有效的索引文档:" + getReader().numDocs());
             // 总共的索引文档
-            logger.info("总共的索引文档:" + getReader().maxDoc());
+            log.info("总共的索引文档:" + getReader().maxDoc());
             // 删掉的索引文档，其实不恰当，应该是在回收站里的索引文档
-            logger.info("删掉的索引文档:" + getReader().numDeletedDocs());
+            log.info("删掉的索引文档:" + getReader().numDeletedDocs());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -208,7 +257,7 @@ public class LuceneServiceImpl
             directory = FSDirectory.open(Paths.get(filePath));
             indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         } catch (IOException e) {
-            logger.error("{}", e);
+            log.error("{}", e);
         }
     }
 }
